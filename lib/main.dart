4d1d9117b9c94cba6eee8250032249af6dbd82b2
thephotogenicbug <navinkumarachari@gmail.com>
2023-02-04 23:36:44 +0530

    First Basic Design

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,24 @@ void main() {
   runApp(MaterialApp(
       title: 'My Flutter App',
       home: Scaffold(
-        appBar: AppBar(title: Text("Dashboard".toUpperCase())),
+        appBar: AppBar(
+          title:
+           Text("Dashboard".toUpperCase())),
+           backgroundColor: Colors.blueGrey,
         body: Center(
-          child: Text(
-            "Hello world",
-            textDirection: TextDirection.ltr,
+          child: Text.rich(
+            TextSpan(
+              text: 'My',
+              children: [
+                TextSpan(
+                    text: 'Flutter',
+                    style:
+                        TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
+                TextSpan(
+                    text: 'App',
+                    style: TextStyle(fontSize: 30.0, color: Colors.blue))
+              ],
+            ),
           ),
         ),
       )));
