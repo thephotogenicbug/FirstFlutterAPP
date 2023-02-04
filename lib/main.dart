acd004b8c39c7dfbb2c2d6352192ba5b8d0dbc8b
thephotogenicbug <navinkumarachari@gmail.com>
2023-02-04 23:44:37 +0530

    Organise Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: 'My Flutter App',
-      home: Scaffold(
-        appBar: AppBar(
-          title:
-           Text("Dashboard".toUpperCase())),
-           backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Text.rich(
-            TextSpan(
-              text: 'My',
-              children: [
-                TextSpan(
-                    text: 'Flutter',
-                    style:
-                        TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
-                TextSpan(
-                    text: 'App',
-                    style: TextStyle(fontSize: 30.0, color: Colors.blue))
-              ],
+  runApp(MyApp());
+}
+
+// Stateless widget do not change there position on there screen once the app is running
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'My Flutter App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Dashboard".toUpperCase(),
             ),
           ),
-        ),
-      )));
+          backgroundColor: Colors.blueGrey,
+          body: Center(
+            child: Text.rich(
+              TextSpan(
+                text: 'My',
+                children: [
+                  TextSpan(
+                      text: 'Flutter',
+                      style: TextStyle(
+                          fontSize: 50.0, fontWeight: FontWeight.bold)),
+                  TextSpan(
+                      text: 'App',
+                      style: TextStyle(fontSize: 30.0, color: Colors.blue))
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
 }
