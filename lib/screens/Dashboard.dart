89cfd53e588bfc04d858841c877d816b8b5de82c
thephotogenicbug <navinkumarachari@gmail.com>
2023-02-05 17:51:07 +0530

    Use Images

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -14,11 +14,13 @@ class Dashboard extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.blueGrey,
-      body: Center(child: Text("Random value is :  ${getNumber()}")),
+      body: Image(
+        image: NetworkImage(
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*'),
+        width: 200.0,
+        height: 800.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
-}
-
-int getNumber() {
-  return Random().nextInt(100);
 }
